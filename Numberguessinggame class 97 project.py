f37d13abf662b9701c3f37f10de31635770d3731
NandanaCRajeev <83021680+NandanaCRajeev@users.noreply.github.com>
2021-11-17 18:57:28 +0530

    Add files via upload

---
+++ Numberguessinggame class 97 project.py
@@ -0,0 +1,23 @@
+import random
+tittle="number guessing game"
+titletwo="Guess a number(between 1&9)"
+tittle3="Enter your guess:-3"
+title4="Yuor guess was too low:Guess a number higher than 3"
+tittle5="Enter your guess:- 4"
+title6="Yuor guess was too low:Guess a number higher than 4"
+tittle7="Enter your guess:- 7"
+title8="Yuor guess was too low:Guess a number higher than 7"
+tittle7="Enter your guess:- 9"
+title9="Coungratulation YOU WON !!!"
+print(tittle)
+print(titletwo)
+print(tittle3)
+print(title4)
+print(tittle5)
+print(title6)
+print(tittle7)
+print(title8)
+print(tittle7)
+print(title9)
+     
+ 
